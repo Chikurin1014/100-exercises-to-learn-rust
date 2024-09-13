0a306a0067d43b462d2d0cdc04f6e9dc7cec0191
Chikurin1014 <chikurin1014@gmail.com>
2024-09-13 12:30:14 +0900

    ✨ solved (04) traits - (11) clone

--- exercises/04_traits/11_clone/src/lib.rs
+++ exercises/04_traits/11_clone/src/lib.rs
@@ -1,8 +1,20 @@
 // TODO: add the necessary `Clone` implementations (and invocations)
 //  to get the code to compile.
 
+// MY_NOTE: You could improve `summary(self)`'s implimention:
+// MY_NOTE: pub fn summary(&self) -> Summary {
+// MY_NOTE:     Summary {
+// MY_NOTE:         title: self.title.clone(),
+// MY_NOTE:         status: self.status.clone(),
+// MY_NOTE:     }
+// MY_NOTE: }
+// MY_NOTE: and then you can write like:
+// MY_NOTE: pub fn summary(ticket: Ticket) -> (Ticket, Summary) {
+// MY_NOTE:     let summary = ticket.summary();
+// MY_NOTE:     (ticket, summary)
+// MY_NOTE: }
 pub fn summary(ticket: Ticket) -> (Ticket, Summary) {
-    (ticket, ticket.summary())
+    (ticket.clone(), ticket.summary())
 }
 
 pub struct Ticket {
@@ -16,6 +28,16 @@ impl Ticket {
         Summary {
             title: self.title,
             status: self.status,
+        }
+    }
+}
+
+impl Clone for Ticket {
+    fn clone(&self) -> Self {
+        Ticket {
+            title: self.title.clone(),
+            description: self.description.clone(),
+            status: self.status.clone(),
         }
     }
 }

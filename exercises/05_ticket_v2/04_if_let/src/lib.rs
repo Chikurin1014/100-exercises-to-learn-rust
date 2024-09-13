e817c5d45d0af1c206b800bdc16bb2908b684e75
Chikurin1014 <chikurin1014@gmail.com>
2024-09-13 14:08:07 +0900

    ✨ solved (05) ticket_v2 - (04) if_let

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -1,14 +1,19 @@
+#[allow(dead_code)]
 enum Shape {
     Circle { radius: f64 },
     Square { border: f64 },
     Rectangle { width: f64, height: f64 },
 }
 
+#[allow(dead_code)]
 impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        todo!()
+        let Self::Circle { radius } = self else {
+            panic!()
+        };
+        *radius
     }
 }
 

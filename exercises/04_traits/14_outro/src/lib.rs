8687e265718eab93f2ba2e906f75f6a56800dd4e
Chikurin1014 <chikurin1014@gmail.com>
2024-09-13 13:35:21 +0900

    ✨ solved (04) traits - (14) outro

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,65 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+pub use saturating_u16::SaturatingU16;
+
+mod saturating_u16 {
+    #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+    pub struct SaturatingU16(u16);
+
+    impl From<u16> for SaturatingU16 {
+        fn from(value: u16) -> Self {
+            Self(value)
+        }
+    }
+    impl From<u8> for SaturatingU16 {
+        fn from(value: u8) -> Self {
+            Self(value.into())
+        }
+    }
+    impl From<&u16> for SaturatingU16 {
+        fn from(value: &u16) -> Self {
+            Self(*value)
+        }
+    }
+    impl From<&u8> for SaturatingU16 {
+        fn from(value: &u8) -> Self {
+            Self((*value).into())
+        }
+    }
+    impl std::ops::Add for SaturatingU16 {
+        type Output = Self;
+        fn add(self, rhs: Self) -> Self::Output {
+            Self(self.0.saturating_add(rhs.0))
+        }
+    }
+    impl std::ops::Add<u16> for SaturatingU16 {
+        type Output = Self;
+        fn add(self, rhs: u16) -> Self::Output {
+            Self(self.0.saturating_add(rhs))
+        }
+    }
+    impl std::ops::Add<&SaturatingU16> for SaturatingU16 {
+        type Output = Self;
+        fn add(self, rhs: &SaturatingU16) -> Self::Output {
+            Self(self.0.saturating_add(rhs.0))
+        }
+    }
+    impl std::ops::Add<&u16> for SaturatingU16 {
+        type Output = Self;
+        fn add(self, rhs: &u16) -> Self::Output {
+            Self(self.0.saturating_add(*rhs))
+        }
+    }
+    impl PartialEq<u16> for SaturatingU16 {
+        fn eq(&self, other: &u16) -> bool {
+            self.0.eq(other)
+        }
+    }
+    impl PartialOrd<u16> for SaturatingU16 {
+        fn partial_cmp(&self, other: &u16) -> Option<std::cmp::Ordering> {
+            self.0.partial_cmp(other)
+        }
+    }
+}
